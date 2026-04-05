80e5b4db31b78017e43e0c7957c51435f2df2718
YSAS4331 <ryo.matsu0619@gmail.com>
2026-04-05 14:32:32 +0900

    Update load.js

--- load.js
+++ load.js
@@ -1,3 +1,5 @@
 import '/Sp3/component/accordion.js';
 import '/Sp3/component/header.js';
 import '/Sp3/component/aside.js';
+import { createIcons, icons } from "https://esm.sh/lucide";
+createIcons({ icons });
