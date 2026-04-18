7ba525270fda30b662207baf3d4ac70203140362
YSAS4331 <ryo.matsu0619@gmail.com>
2026-04-18 11:57:50 +0900

    Update aside.js

--- component/aside.js
+++ component/aside.js
@@ -254,7 +254,7 @@ class aside extends HTMLElement {
         box.slot = 'item';
 
         items.forEach(item => {
-          if (!item) continue;
+          if (!item) return;
           const a = document.createElement('a');
           if (label === '武器別') {
             a.href = `/Sp3/weapons/?weapon=${encodeURIComponent(item)}`;
