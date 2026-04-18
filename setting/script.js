8b2df10e709ec67cd163ac91562bb4bddeda778f
YSAS4331 <ryo.matsu0619@gmail.com>
2026-04-18 09:28:48 +0900

    Update script.js

--- setting/script.js
+++ setting/script.js
@@ -2,4 +2,6 @@ export function init() {
   import("https://esm.sh/lucide").then(({ createIcons, icons }) => {
     createIcons({ icons });
   });
+
+  const $ = id => document.getElementById(id);
 }
