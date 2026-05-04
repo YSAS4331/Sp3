c2ea8325637e34cf8630eebe00a8b3710874b380
YSAS4331 <ryo.matsu0619@gmail.com>
2026-05-04 17:44:50 +0900

    Update load.js

--- load.js
+++ load.js
@@ -4,3 +4,39 @@ import '/Sp3/component/aside.js';
 import '/Sp3/SETTING.js';
 import { createIcons, icons } from "https://esm.sh/lucide";
 createIcons({ icons });
+
+import { init } from '/Sp3/db.js';
+init();
+
+(async function loadInitialDatas() {
+  await Promise.all([
+    loadJsonWithVersion("/Sp3/datas/ids.json", "ids", window.SETTINGS.IDS_VERSION),
+    loadJsonWithVersion("/Sp3/datas/translate.json", "translate", window.SETTINGS.TRANSLATE_VERSION)
+  ]);
+})();
+
+async function loadJsonWithVersion(url, key, requiredVersion) {
+  const existing = await window.SetDB.getItem(key);
+
+  if (existing && existing.VERSION === requiredVersion) {
+    console.log(`[${key}] version OK → skip`);
+    return;
+  }
+
+  const res = await fetch(url);
+  if (!res.ok) {
+    console.error(`Failed to fetch ${url}`);
+    return;
+  }
+
+  const data = await res.json();
+
+  // 保存形式：{ VERSION, data }
+  const saveObj = {
+    VERSION: requiredVersion,
+    data
+  };
+
+  await window.SetDB.setItem(key, saveObj);
+  console.log(`[${key}] saved with version ${requiredVersion}`);
+}
